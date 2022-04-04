334a7f63530d1cc7216084fbac27fc46cee0954a
Emmanuel Ramon <emmanuelrlima@gmail.com>
2022-04-03 21:24:18 -0300

    funcao jump criada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 const dino = document.querySelector('.dino');
 
 function handleKeyUp(event) {
-    if(event.key === " ") {
-        console.log('Pular');
+    if(event.key === ' ') {
+        jump()
     }
+}
+
+function jump() {
+    let position = 0;
+    let up = false;
+
+    let upInterval = setInterval( () => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+            let downInterval = setInterval( () => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
